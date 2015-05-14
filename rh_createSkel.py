2f7fd9129e70e8de0bd757e93e0376651b61ee43
RyugasakiHu <frostnova53@gmail.com>
2015-05-14 10:09:47 +0800

    Update rh_createSkel.py

--- rh_createSkel.py
+++ rh_createSkel.py
@@ -6,65 +6,60 @@
 #Description:
 
 import maya.cmds as mc
-
-def rh_createSkelGeo(input):
-    input = mc.ls(sl = 1,type='joint')
-    if len(input) == 0:
-        print 'please select joint!'
-    if len(input) > 0:
-        for joints in input: 
-           
-        #print len(input)
-        #children = mc.listRelatives(input,c=1,type='joint') 
-        #for child in children:
-        #print child
-                
-        #for joints in input:
-        #print joints[]
-    
+#joints = mc.ls(type='joint')
+#radius = mc.getAttr(joints[0] + '.radius')
+#print radius
+#print tx
 def createSkelGeolength():
-    joints = mc.ls(sl=1,type='joint')
-    cube = mc.polyCube(n='joint1' + '_geo',sx=1,sy=1,sz=1) 
-    mc.parent(cube[0],joints)
-    tx = mc.getAttr(joints[0] + '.tx')
-    ty = mc.getAttr(joints[0] + '.ty')
-    tz = mc.getAttr(joints[0] + '.tz')
-    
-    abtx = abs(tx)
-    abty = abs(ty)
-    abtz = abs(tz)
-    mc.setAttr(cube[0] + '.tx',tx * 0.5)
-    mc.setAttr(cube[0] + '.ty',ty * 0.5)
-    mc.setAttr(cube[0] + '.tz',tz * 0.5)
-    
-    mc.setAttr(cube[0] + '.r',0,0,0)
-    
-    radius = mc.getAttr(joints[0] + '.radius')
-    
-    if abtx > 0.001:
-        sx = abtx
-    else :    
-        sx = radius
+    a = 1
+    count = 0
+    joints = mc.ls(sl=1,type='joint')       
+    realchild = mc.listRelatives(c=1) 
+    for child in joints:        
+        cube = mc.polyCube(n='joints' + '_geo0' + str(a),sx=1,sy=1,sz=1)
+        mc.parent(cube,child)
+        if count < (len(joints)-1):
+            tx = mc.getAttr(realchild[count] + '.tx')
+            ty = mc.getAttr(realchild[count] + '.ty')
+            tz = mc.getAttr(realchild[count] + '.tz')
+                    
+            abtx = abs(tx)
+            abty = abs(ty)
+            abtz = abs(tz)
+            
+            mc.setAttr(cube[0] + '.tx',tx * 0.5)
+            mc.setAttr(cube[0] + '.ty',ty * 0.5)
+            mc.setAttr(cube[0] + '.tz',tz * 0.5)
+            mc.setAttr(cube[0] + '.r',0,0,0)
         
-    if abty > 0.001:
-        sy = abty
-    else :    
-        sy = radius
+            radius = mc.getAttr(child + '.radius')
         
-    if abtz > 0.001:
-        sz = abtz
-    else :    
-        sz = radius            
-    
-    mc.setAttr(cube[0] + '.sx',sx)
-    mc.setAttr(cube[0] + '.sy',sy)
-    mc.setAttr(cube[0] + '.sz',sz)
-    
+            if abtx > 0.001:
+                sx = abtx
+            else :    
+                sx = radius
+            
+            if abty > 0.001:
+                sy = abty
+            else :    
+                sy = radius
+       
+            if abtz > 0.001:
+                sz = abtz
+            else :    
+                sz = radius            
+        
+            mc.setAttr(cube[0] + '.sx',sx)
+            mc.setAttr(cube[0] + '.sy',sy)
+            mc.setAttr(cube[0] + '.sz',sz)
 
-    mc.select(cube[0])
-    mc.makeIdentity(cube[0],t=1,s=1,r=1,a=1)
-
-    
-createSkelGeolength()    
-    
-rh_createSkelGeo(input)    
+            #clear history
+            mc.select(cube[0])
+            mc.makeIdentity(cube[0],t=1,s=1,r=1,a=1)
+            
+            a += 1
+            count += 1
+    #break the cycle delete the last one        
+    mc.delete('joints' + '_geo0' + str(a))
+ 
+createSkelGeolength()        
