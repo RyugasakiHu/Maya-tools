7375533cb9ec6bb0b4ba621e3ce1c750887fc905
RyugasakiHu <frostnova53@gmail.com>
2015-05-20 23:35:07 +0800

    Update rh_Ribbon.py

--- rh_Ribbon.py
+++ rh_Ribbon.py
@@ -21,10 +21,10 @@ def RH_Ribbon():
     mc.textFieldGrp('RH_R_NameTFG',label = 'Ribbon_name:', text = 'Ribbon', ed = True)
     mc.columnLayout(adjustableColumn = True)
     #width
-    mc.floatSliderGrp('RH_R_WidthFSG',label = "Width:",f = True,min = 0.1,max = 5.0,fmn = 0.1,fmx = 100,v = 0.1)
+    mc.floatSliderGrp('RH_R_WidthFSG',label = 'Width:',f = True,min = 0.1,max = 5.0,fmn = 0.1,fmx = 100,v = 0.1)
     mc.columnLayout(adjustableColumn = True)
     #lenth
-    mc.floatSliderGrp('RH_R_LengthFSG',label = "Length:",f = True,min = 0,max = 50,fmn = 0,fmx = 100,v = 5)
+    mc.floatSliderGrp('RH_R_LengthFSG',label = 'Length:',f = True,min = 0,max = 50,fmn = 0,fmx = 100,v = 5)
     mc.columnLayout(adjustableColumn = True)
     
     #U patch intSliderGrp 
@@ -36,7 +36,7 @@ def RH_Ribbon():
     mc.columnLayout(adjustableColumn = True)
     
     #radioButtonGrp 
-    mc.radioButtonGrp('RH_R_ORBG',nrb = 2,label = 'Ribbon options:',la2 = ['Follicle','Non - Follicle'],sl = 1)	
+    mc.radioButtonGrp('RH_R_ORBG',nrb = 2,label = 'Ribbon options:',la2 = ['Follicle','Non - Follicle'],sl = 2)	
     mc.columnLayout(adjustableColumn = True)
     mc.button(label = 'Requesting for 105 shells!', command = 'Barrage()')
     mc.columnLayout(adjustableColumn = True)
@@ -71,7 +71,7 @@ def Barrage():
     #CREATE THE HAIR FOLLICLES	OR POINT ON SURFACE INFO NODES ACCORDING TO THE OPTIONS SELECTED---
     if ribbonOptionVal == 1:
         mc.select(ribbonGeo[0],r = 1)
-        mc.CreateHair(VVal,UVal,10,0,0,0,0,5,0,2,1,1)
+        mc.CreateHair(VVal,UVal,10,0,0,0,0,5,0,1,1,1)
         selFols = mc.select(ribbonName + '_Rbbn01_geo_01' + '*Follicle*',r = 1)
         folGrp = mc.group(n = ribbonName + 'Rbbn01_fol_grp')
         mc.parent(w = 1)
@@ -87,4 +87,74 @@ def Barrage():
         
         #CREATE JOINTS SNAPPED AND PARENTED TO THE FOLLICLE---
         mc.select(cl = 1)    
+        for a in range(len(sel)/2):
+            b = a + 1
+            mc.joint(n = ribbonName + '_Rbbn0' + str(b) + '_jj',p = (0,0,0))
+            mc.parent(ribbonName + '_Rbbn0' + str(b) + '_jj',ribbonName + '_Rbbn0' + str(b) + '_fol',r = True)
+            mc.select(cl = 1)
+            
+    #FOR POINT ON SURFACE METHOD---     
+    if ribbonOptionVal == 2:
+        
+        #ON THE BASIS OF POINT ON SURFACE INFO NODE---	
+        mc.select(ribbonGeo[0])
+        mc.pickWalk(d = 'down')
+        ribbonGeoShape = mc.ls(sl = 1)
+        
+        #VERIFICATION
+        if UVal > VVal:
+            
+            no  = UVal
+            
+        if VVal > UVal:    
+        
+            no  = VVal
+        
+        ribbonPosGrpVal = mc.group(em = True,n = ribbonName + '_Rbbn01_pos_grp')
+        mc.xform(ws = True,piv = (0,0,0))    
+        
+        #EXECUTE THE MAIN LOOP---
+        for i in range(no):
+            POSINode = mc.createNode('pointOnSurfaceInfo')
+            newName = ribbonName + '_Rbbn0' + str(i) + '_posi'
+            POSIName = mc.rename(POSINode,newName)
+            
+            mc.connectAttr(ribbonGeoShape[0] + '.worldSpace[0]',POSIName + '.inputSurface',f = 1)
+        
+            if VVal > UVal :
+                normVVal = abs((1.0/no) *i)
+                mc.setAttr(POSIName + '.parameterV',normVVal)
+                mc.setAttr(POSIName + '.parameterU',0.5)
+            
+            if UVal > VVal :
+                normUVal = abs((1.0/no) *i)
+                mc.setAttr(POSIName + '.parameterV',0.5)
+                mc.setAttr(POSIName + '.parameterU',normUVal)
+                
+            posGrp = mc.group(em = True,n = POSIName + 'Rbbn0'+ str(i) + '_pos') 
+            upGrp = mc.group(em = True,n = POSIName + 'Rbbn0'+ str(i) + '_up')  
+            aimGrp = mc.group(em = True,n = POSIName + 'Rbbn0'+ str(i) + '_aim') 
+        
+            mc.parent(upGrp,posGrp)
+            mc.parent(aimGrp,posGrp)
+            mc.parent(posGrp,ribbonPosGrpVal)
+            
+            mc.connectAttr(POSIName + '.position',posGrp + '.translate',f = 1)
+            mc.connectAttr(POSIName + '.tangentU',upGrp + '.translate',f = 1)
+            mc.connectAttr(POSIName + '.tangentV',aimGrp + '.translate',f = 1)
+            
+            mc.select(cl = 1)
+            
+            JJ = mc.joint(p = (0,0,0),n = ribbonName + 'Rbbn0' + str(i) + '_jj')
+            mc.parent(JJ,posGrp,r = 1)
+            
+            mc.select(JJ,r = 1)
+            JJOff = mc.group(n = ribbonName + "Rbbn0" + str(i) + "_jj_off")
+            
+            mc.aimConstraint(aimGrp,JJOff,o = (0,0,0),w = 1,aim = (0,0,1),wut = 'object',wuo = upGrp,n = 'aimConstraint_0' + str(i))
+
+    #CREATE SOME TEMPORARY CLUSTERS TO PLACE THE POS LOCATORS---
+    
+    
+    	                                
 RH_Ribbon()    
