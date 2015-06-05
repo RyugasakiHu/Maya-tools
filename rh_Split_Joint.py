5aeeed7638b7611189be5742c0cfb7d24964eec8
RyugasakiHu <frostnova53@gmail.com>
2015-06-05 11:46:22 +0800

    Update and rename Split_Joint.py to rh_Split_Joint.py

--- rh_Split_Joint.py
+++ rh_Split_Joint.py
@@ -1,5 +1,4 @@
-#for i in range(0,10):
-#    print "a"
+
 import maya.cmds as mc
 
 def RH_Split():
@@ -15,22 +14,30 @@ def RH_Split():
     mc.intFieldGrp('RH_R_NameTFG',numberOfFields=1,label='Split_Joint_Num:', value1=0)
     #mc.intField('RH_R_NameTFG',label='Split_Joint_Num:',value=0 )
     mc.columnLayout(adjustableColumn=True)
-    mc.button(label='Jawoll', command='Jawoll()')
+    mc.button(label='Achtung', command='Achtung()')
     mc.columnLayout(adjustableColumn=True)
     mc.showWindow(winName)
     
-def Jawoll():
+def Achtung():
     #get info
     num_jnt=mc.intFieldGrp('RH_R_NameTFG',q=True,v=True)
     a1=mc.ls(sl=1)
     a2=mc.listRelatives(a1,c=1)
     b1=mc.xform(a1,q=1,ws=1,t=1)
     b2=mc.xform(a2,q=1,ws=1,t=1)
+    #unparent
+    mc.parent(a2,w=1)
     
-    #mc.parent(a2,w=1)
-    
-    for i in range(num_jnt):
-        print 'a'
-    print type(num_jnt),a1,a2,b1,b2
- 
+    for i in range(0,num_jnt[0]):
+       mc.select(a1,r=1) 
+       b3=[(b1[0] - b2[0])*(i + 1)/(num_jnt[0] + 1) + b2[0],
+       (b1[1] - b2[1])*(i + 1)/(num_jnt[0] + 1) + b2[1],
+       (b1[2] - b2[2])*(i + 1)/(num_jnt[0] + 1) + b2[2]]
+       radius = mc.getAttr(a1[0] + '.radius')
+       mc.joint(rad = radius,p=(b3[0],b3[1],b3[2]),n=a1[0]+"_insert_"+str(num_jnt[0]-i))
+       if i == 0:
+           mc.parent(a2[0],a1[0]+"_insert_"+str(num_jnt[0]-i))
+       else :    
+           mc.parent(a1[0]+"_insert_"+str(num_jnt[0]-i+1),a1[0]+"_insert_"+str(num_jnt[0]-i))
+           
 RH_Split()    
