74e8960c1b9315110809e7fd8ae913cbf6a4c059
RyugasakiHu <frostnova53@gmail.com>
2015-07-21 23:19:36 +0800

    Update rh_Ribbon.py

--- rh_Ribbon.py
+++ rh_Ribbon.py
@@ -7,7 +7,7 @@
 #Description: Enter the name Use this Tool to automatically create a ribbon system...
 #AAAAAAAAttention: Only tested in maya 2013 and 2015 version,
                  # You need to change the hash manually 
-                 # At line 77 or 79 base on the version of your maya!!!
+                 # At line 74 or 77 base on the version of your maya!!!
 
 import maya.cmds as mc
 
@@ -38,9 +38,7 @@ def RH_Ribbon():
     mc.intSliderGrp('RH_R_VISG',label = 'V patches:',field = True,min = 1,max = 10,fmn = 1,fmx = 100,v = 5)
     mc.columnLayout(adjustableColumn = True)
     
-    #radioButtonGrp 
-    mc.radioButtonGrp('RH_R_ORBG',nrb = 2,label = 'Ribbon options:',la2 = ['Follicle','Non - Follicle'],sl = 1)	
-    mc.columnLayout(adjustableColumn = True)
+    #button
     mc.button(label = 'Requesting for 105 shells!', command = 'Barrage()')
     mc.columnLayout(adjustableColumn = True)
     #Show the window
@@ -48,13 +46,13 @@ def RH_Ribbon():
     mc.window(winName, edit = True, width = 378, height = 210)
     
 def Barrage():
+    global UVal,VVal
     #collection coordinate
     ribbonName = mc.textFieldGrp('RH_R_NameTFG', tx = True,q = True)
     widthVal = mc.floatSliderGrp('RH_R_WidthFSG',q = True,v = True)
     lengthVal = mc.floatSliderGrp('RH_R_LengthFSG',q = True,v = True)
     UVal = mc.intSliderGrp('RH_R_UISG',q = True,v = True)
     VVal = mc.intSliderGrp('RH_R_VISG',q = True,v = True)
-    ribbonOptionVal = mc.radioButtonGrp('RH_R_ORBG',q = True,sl = True)
     
     #create burbs plane
     ribbonGeo = mc.nurbsPlane(p = (0,0,0),ax = (0,1,0),w = widthVal,lr = lengthVal,d = 3,u = UVal,v = VVal,ch = 1,n = (ribbonName + '_Rbbn01_geo_01_'))
@@ -70,92 +68,33 @@ def Barrage():
     mc.DeleteHistory
     mc.delete(ch = 1)
     
-    #CREATE THE HAIR FOLLICLES	OR POINT ON SURFACE INFO NODES ACCORDING TO THE OPTIONS SELECTED---
-    if ribbonOptionVal == 1:
-        mc.select(ribbonGeo[0],r = 1)
-        #2013
-        mc.CreateHair(VVal,UVal,10,0,0,0,0,5,0,1,1,1)
-        #2015
-        #mc.CreateHair(VVal,UVal,0,0,0,0,0,5,0,3,1,1)
-        
-        selFols = mc.select(ribbonName + '_Rbbn01_geo_01' + '*Follicle*',r = 1)
-        folGrp = mc.group(n = ribbonName + '_Rbbn01_fol_grp')
-        mc.parent(w = 1)
-        mc.delete('hairSystem*')
-        
-        selFols = mc.select(ribbonName + '_Rbbn01_geo_01' + '*Follicle*',r = 1)
-        sel = mc.ls(sl = 1)
-        
-        for i in range(len(sel)/2):
-            j = i + 1
-            newName = (ribbonName + '_Rbbn0' + str(j) + '_fol')
-            mc.rename(sel[i],newName)
-        
-        #CREATE JOINTS SNAPPED AND PARENTED TO THE FOLLICLE---
-        mc.select(cl = 1)    
-        for a in range(len(sel)/2):
-            b = a + 1
-            mc.joint(n = ribbonName + '_Rbbn0' + str(b) + '_jj',p = (0,0,0),rad = min(widthVal,lengthVal) * .25)
-            mc.parent(ribbonName + '_Rbbn0' + str(b) + '_jj',ribbonName + '_Rbbn0' + str(b) + '_fol',r = True)
-            mc.select(cl = 1)
-            
-    #FOR POINT ON SURFACE METHOD---     
-    if ribbonOptionVal == 2:
-        
-        #ON THE BASIS OF POINT ON SURFACE INFO NODE---	
-        mc.select(ribbonGeo[0])
-        mc.pickWalk(d = 'down')
-        ribbonGeoShape = mc.ls(sl = 1)
-        
-        #VERIFICATION
-        if UVal > VVal:            
-            no  = UVal
-            
-        if VVal > UVal:            
-            no  = VVal
-        
-        ribbonPosGrpVal = mc.group(em = True,n = ribbonName + '_Rbbn01_pos_grp')
-        mc.xform(ws = True,piv = (0,0,0))    
-        
-        #EXECUTE THE MAIN LOOP---
-        for i in range(no):
-            POSINode = mc.createNode('pointOnSurfaceInfo')
-            newName = ribbonName + '_Rbbn0' + str(i) + '_posi'
-            POSIName = mc.rename(POSINode,newName)
-            
-            mc.connectAttr(ribbonGeoShape[0] + '.worldSpace[0]',POSIName + '.inputSurface',f = 1)
-        
-            if VVal > UVal :
-                normVVal = abs((1.0/no) *i)
-                mc.setAttr(POSIName + '.parameterV',normVVal)
-                mc.setAttr(POSIName + '.parameterU',0.5)
-            
-            if UVal > VVal :
-                normUVal = abs((1.0/no) *i)
-                mc.setAttr(POSIName + '.parameterV',0.5)
-                mc.setAttr(POSIName + '.parameterU',normUVal)
-                
-            posGrp = mc.group(em = True,n = POSIName + 'Rbbn0'+ str(i) + '_pos') 
-            upGrp = mc.group(em = True,n = POSIName + 'Rbbn0'+ str(i) + '_up')  
-            aimGrp = mc.group(em = True,n = POSIName + 'Rbbn0'+ str(i) + '_aim') 
-        
-            mc.parent(upGrp,posGrp)
-            mc.parent(aimGrp,posGrp)
-            mc.parent(posGrp,ribbonPosGrpVal)
-            
-            mc.connectAttr(POSIName + '.position',posGrp + '.translate',f = 1)
-            mc.connectAttr(POSIName + '.tangentU',upGrp + '.translate',f = 1)
-            mc.connectAttr(POSIName + '.tangentV',aimGrp + '.translate',f = 1)
-            
-            mc.select(cl = 1)
-            
-            JJ = mc.joint(p = (0,0,0),rad = min(widthVal,lengthVal) * .25,n = ribbonName + 'Rbbn0' + str(i) + '_jj')
-            mc.parent(JJ,posGrp,r = 1)
-            
-            mc.select(JJ,r = 1)
-            JJOff = mc.group(n = ribbonName + 'Rbbn0' + str(i) + '_jj_off')
-            
-            mc.aimConstraint(aimGrp,JJOff,o = (0,0,0),w = 1,aim = (0,0,1),wut = 'object',wuo = upGrp,n = 'aimConstraint_0' + str(i))
+    #CREATE THE HAIR FOLLICLES
+    mc.select(ribbonGeo[0],r = 1)
+    #2013
+    #mc.CreateHair(VVal,UVal,10,0,0,0,0,5,0,1,1,1)
+    #2015
+    mc.CreateHair(VVal,UVal,0,0,0,0,0,5,0,3,1,1)
+    
+    selFols = mc.select(ribbonName + '_Rbbn01_geo_01' + '*Follicle*',r = 1)
+    folGrp = mc.group(n = ribbonName + '_Rbbn01_fol_grp')
+    mc.parent(w = 1)
+    mc.delete('hairSystem*')
+    
+    selFols = mc.select(ribbonName + '_Rbbn01_geo_01' + '*Follicle*',r = 1)
+    sel = mc.ls(sl = 1)
+    
+    for i in range(len(sel)/2):
+        j = i + 1
+        newName = (ribbonName + '_Rbbn0' + str(j) + '_fol')
+        mc.rename(sel[i],newName)
+    
+    #CREATE JOINTS SNAPPED AND PARENTED TO THE FOLLICLE---
+    mc.select(cl = 1)    
+    for a in range(len(sel)/2):
+        b = a + 1
+        mc.joint(n = ribbonName + '_Rbbn0' + str(b) + '_jj',p = (0,0,0),rad = min(widthVal,lengthVal) * .25)
+        mc.parent(ribbonName + '_Rbbn0' + str(b) + '_jj',ribbonName + '_Rbbn0' + str(b) + '_fol',r = True)
+        mc.select(cl = 1)
 
     #CREATE SOME TEMPORARY CLUSTERS TO PLACE THE POS LOCATORS---
     if UVal > VVal:
@@ -268,15 +207,11 @@ def Barrage():
     mc.rename(ribbonName + '_Rbbn01_geo_01_',ribbonName + '_Rbbn01_geo_01')
     
     #GROUP THEM ALL
-    if ribbonOptionVal == 1:
-        mc.delete('pfxHair1')
-        mc.delete('nucleus1')
-        mc.group(ribbonName + '_Rbbn01_fol_grp',ribbonName + '_Rbbn01_geo_01',ribbonName + '_RbbnSp01_pos',ribbonName + '_RbbnMp01_pos',ribbonName + '_RbbnEp01_pos',n = ribbonName + "_Rbbn01_grp")
-        mc.xform(os = 1,piv = (0,0,0))
-            
-    if ribbonOptionVal == 2:
-        mc.group(ribbonName + '_Rbbn01_pos_grp',ribbonName + '_Rbbn01_geo_01',ribbonName + '_RbbnSp01_pos',ribbonName + '_RbbnMp01_pos',ribbonName + '_RbbnEp01_pos',n = ribbonName + "_Rbbn01_grp")
-        mc.xform(os = 1,piv = (0,0,0))
+
+    mc.delete('nucleus1')
+    mc.group(ribbonName + '_Rbbn01_fol_grp',ribbonName + '_Rbbn01_geo_01',ribbonName + '_RbbnSp01_pos',ribbonName + '_RbbnMp01_pos',ribbonName + '_RbbnEp01_pos',n = ribbonName + "_Rbbn01_grp")
+    mc.xform(os = 1,piv = (0,0,0))
+           
         
     print 'Target Neutralize'    
 RH_Ribbon()    
