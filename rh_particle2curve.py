27a052617361302c1b4e31c101a9dfa9b509f785
RyugasakiHu <frostnova53@gmail.com>
2016-06-26 12:57:00 +0800

    Update rh_particle2curve.py

--- rh_particle2curve.py
+++ rh_particle2curve.py
@@ -2,23 +2,32 @@ import pymel.core as pm
 
 ptList = []
 curveList = []
+countList = []
 
 part = pm.ls(sl = 1)[0]
 timeMin = pm.playbackOptions(q = 1,min = 1)
 timeMax = pm.playbackOptions(q = 1,max = 1)
-countList = []
+
+def getUniqueName(basename,suffix):
+    
+    security = 1000    
+    name = basename + '_' + str(0) +  '_' + suffix       
+    i = 0
+    while (pm.objExists(name) == 1):
+        if(i < security):
+            i += 1
+            name = basename + '_' + str(i) +  '_' + suffix            
+    return name      
     
 def createCurve(particlePoint):
     ptPos = pm.getParticleAttr(particlePoint,at = 'position')
-    ptCurve = pm.curve(d = 3,p = ptPos[0:3])
+    ptCurve = pm.curve(d = 3,p = ptPos[0:3],n = getUniqueName(part,'cur'))
     curveList.append(ptCurve)
-    print ptPos
+
     
-def addCurvePoint(particlePoint):
+def addCurvePoint(particlePoint,createdCurve):
     ptPos = pm.getParticleAttr(particlePoint,at = 'position')
-    ptCurve = pm.listConnections(particlePoint, source = True, destination = False, connections = False, plugs = True )
-    print ptCurve
-    #pm.curve(a = 1,p = ptPos[0:3])   
+    pm.curve(createdCurve,a = 1,p = ptPos[0:3])   
         
 def loopTime():
     for time in range(int(timeMin),int(timeMax)):
@@ -29,27 +38,19 @@ def loopTime():
                 ptList.append(pt)
                 createCurve(pt)
             else :
-                addCurvePoint(pt)           
-
-        pm.currentTime(time + 1)
-        print ptList   
-  
-#    for pt in ptList:
-#        ptPos = pm.getParticleAttr(pt,at = 'position')
-#        ptCurve = pm.curve(d = 3,p = ptPos[0:3])
-#        curveList.append(ptCurve)
-    
-def getUniqueName():
-    pass    
+                addCurvePoint(pt,curveList[num])
+        pm.currentTime(time + 1) 
 
 if pm.currentTime() != timeMin :
     pm.currentTime(timeMin)
     loopTime()
     
 else :
-    loopTime()
-#    pm.currentTime(time + 1)
-#    for num,pt in enumerate(ptList):
-#        ptPos = pm.getParticleAttr(pt,at = 'position')
-#        pm.curve(curveList[0],a = 1,p = ptPos)
-        
+    loopTime()   
+
+curveGrp = pm.group(em = 1,n = getUniqueName(part,'grp'))
+
+for curve in curveList:
+    curve.setParent(curveGrp)
+
+         
